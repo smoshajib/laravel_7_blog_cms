e43303210f57fcc6e293a6f0a955749ffa01689a
Smo Shjaib <smoshajib100@gmail.com>
2021-08-02 15:25:51 +0600

    update post pages galllery

--- resources/views/admin/page/edit.blade.php
+++ resources/views/admin/page/edit.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.admin_master')
 
 @section('content')
     <div class="container">
@@ -54,7 +54,7 @@
     </div>
 @endsection
 
-@section('javascript')
+@section('script')
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <script src="https://cdn.ckeditor.com/4.14.0/standard/ckeditor.js"></script>
 
