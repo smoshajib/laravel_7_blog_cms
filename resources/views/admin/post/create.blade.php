b4ff2b448690bc20eda67ef051386526af6af2a4
Smo Shjaib <smoshajib100@gmail.com>
2021-08-02 15:21:51 +0600

    Post section updated

--- resources/views/admin/post/create.blade.php
+++ resources/views/admin/post/create.blade.php
@@ -1,6 +1,6 @@
-@extends('layouts.app')
+@extends('layouts.admin_master')
 
-@section('stylesheet')
+@section('style')
     <link href="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/css/select2.min.css" rel="stylesheet"/>
 @endsection
 
@@ -63,7 +63,7 @@
     </div>
 @endsection
 
-@section('javascript')
+@section('script')
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"
             integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <script src="https://cdn.ckeditor.com/4.14.0/standard/ckeditor.js"></script>
